f0379d642f697af6491aa89cca87ee47b27efa1c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-01 17:08:59 -0300

    anda !! falta el boton de agregar eventos

--- App.js
+++ App.js
@@ -3,16 +3,33 @@ import { StyleSheet, View, Text, Button, Platform } from 'react-native';
 import * as Calendar from 'expo-calendar';
 
 export default function App() {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Calendar.requestCalendarPermissionsAsync();
-      if (status === 'granted') {
-        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
-        console.log('Here are all your calendars:');
-        console.log(calendars);
+useEffect(() => {
+  (async () => {
+    const { status } = await Calendar.requestCalendarPermissionsAsync();
+    if (status === 'granted') {
+      const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+      console.log('Here are all your calendars:');
+      console.log(calendars);
+
+      if (calendars.length > 0) {
+        const calendarId = calendars[4].id; // Choose the first calendar
+
+        const startDate = new Date(); // Start date
+        const endDate = new Date(); // End date
+        endDate.setDate(endDate.getDate() + 40); // For example, fetch events for the next 7 days
+
+        console.log('Calendar ID:', calendarId);
+        console.log('Start Date:', startDate);
+        console.log('End Date:', endDate);
+
+        const events = await Calendar.getEventsAsync([calendarId], startDate, endDate);
+        console.log('Here are the events for the chosen calendar:');
+        console.log(events);
       }
-    })();
-  }, []);
+    }
+  })();
+}, []);
+
 
   return (
     <View style={styles.container}>
