b9d22320247142798bc76a9b6dd0984f1f317609
Agustin Sirotinsky <agussiro06@gmail.com>
2023-11-01 22:28:03 -0300

    boton de crear deberia andar pero crashea

--- App.js
+++ App.js
@@ -8,65 +8,52 @@ useEffect(() => {
     const { status } = await Calendar.requestCalendarPermissionsAsync();
     if (status === 'granted') {
       const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
-      console.log('Here are all your calendars:');
-      console.log(calendars);
+      console.log('Calendario seleccionado:');
+      console.log(calendars[4]);
 
       if (calendars.length > 0) {
-        const calendarId = calendars[4].id; // Choose the first calendar
+        const calendarId = calendars[4].id; // Elije el calendario que quieras
 
-        const startDate = new Date(); // Start date
-        const endDate = new Date(); // End date
-        endDate.setDate(endDate.getDate() + 40); // For example, fetch events for the next 7 days
+        const startDate = new Date(); // Fecha inicial
+        const endDate = new Date(); // Fecha final
+        endDate.setDate(endDate.getDate() + 7); // Trae los eventos de los primeros 7 dias
 
         console.log('Calendar ID:', calendarId);
         console.log('Start Date:', startDate);
         console.log('End Date:', endDate);
 
         const events = await Calendar.getEventsAsync([calendarId], startDate, endDate);
-        console.log('Here are the events for the chosen calendar:');
+        console.log('Eventos de los proximos 7 dias del calendairo elegido:');
         console.log(events);
       }
     }
   })();
 }, []);
+async function createEvent() {
+  const eventDetails = {
+    title: 'EVENTO CREADO POR CALENDARIO QUEE',
+    startDate: new Date(), // Add a valid start date
+    endDate: new Date(), // Add a valid end date
+  };
 
+  const eventID = await Calendar.createEventAsync(5, eventDetails);
 
-  return (
-    <View style={styles.container}>
-      <Text>Calendar Module Example</Text>
-      <Button title="Create a new calendar" onPress={createCalendar} />
-    </View>
-  );
+  console.log(`Your new event ID is: ${eventID}`);
 }
 
-async function getDefaultCalendarSource() {
-  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
-  return defaultCalendar.source;
-}
-
-async function createCalendar() {
-  const defaultCalendarSource =
-    Platform.OS === 'ios'
-      ? await getDefaultCalendarSource()
-      : { isLocalAccount: true, name: 'Expo Calendar' };
-  const newCalendarID = await Calendar.createCalendarAsync({
-    title: 'Expo Calendar',
-    color: 'blue',
-    entityType: Calendar.EntityTypes.EVENT,
-    sourceId: defaultCalendarSource.id,
-    source: defaultCalendarSource,
-    name: 'internalCalendarName',
-    ownerAccount: 'personal',
-    accessLevel: Calendar.CalendarAccessLevel.OWNER,
-  });
-  console.log(`Your new calendar ID is: ${newCalendarID}`);
+return (
+  <View style={styles.container}>
+    <Text>Calendar Module Example</Text>
+    <Button title="Create a new event" onPress={createEvent} />
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
 });
